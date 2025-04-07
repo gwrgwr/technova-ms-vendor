b27be3f77fe9b78d68073415cdfe6b05bba3bf8a
gwrgwr <murilomassa72@gmail.com>
2025-04-07 15:23:23 -0300

    first commit

---
+++ src/main/java/com/technova/msvendor/MsVendorApplication.java
@@ -0,0 +1,13 @@
+package com.technova.msvendor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsVendorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsVendorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/technova/msvendor/MsVendorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technova.msvendor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsVendorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
